c03091ae6caf796044d897cf9591b23344454a60
buhidar <mbuhidar@gmail.com>
2019-01-12 17:42:34 -0500

    Added complex objects exercise solution.

---
+++ complexObjects.js
@@ -0,0 +1,25 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+  // Add record here
+
+,
+  {
+    "artist": "Clutch",
+    "title": "Clutch",
+    "release_year": 2012,
+    "formats": [
+      "CD",
+      "LP"
+      ]
+  }
+];
